89edbce085ff3389f6a61c263fc43ddef2849c37
Olivier Matis <3273625+Guruumeditation@users.noreply.github.com>
2021-03-14 14:26:26 +0100

    Ajoutez des fichiers projet.

---
+++ TestBlazorHttp.Api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestBlazorHttp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<string> GetResult([FromBody] Foo foo)
+        {
+            return new OkObjectResult($"Hello {foo.FirstName} {foo.LastName}");
+        }
+    }
+}
---
+++ TestBlazorHttp.Api/Foo.cs
@@ -0,0 +1,8 @@
+﻿namespace TestBlazorHttp.Api
+{
+    public class Foo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
